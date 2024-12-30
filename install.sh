870ae564db21f06c39c5db8d0691b91d152d6d74
Jorge Menjivar <jorgemenjivar1997@gmail.com>
2024-12-30 14:21:59 -0800

    release and installation bug fixes

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+set -e
+
 abort() {
   printf "%s\n" "$@" >&2
   exit 1
 }
 
-echo "Installing Secrets Machine to $HOME/.local/bin"
-
 SM_DIR="$HOME/.local/bin"
 SM_PATH="${SM_DIR}/sm"
+
+echo "Installing Secrets Machine to $SM_DIR"
 
 OS="$(/usr/bin/uname -s)"
 ARCH="$(/usr/bin/uname -m)"
@@ -20,7 +22,7 @@ ASSET_NAME="secrets-machine-${ARCH}-${OS}"
 # Get the latest release information
 RELEASE_INFO=$(curl -fsSL https://api.github.com/repos/generative-ai-inc/secrets-machine/releases/latest)
 
-# # Extract the asset download URL
+# Extract the asset download URL
 ASSET_URL=$(echo $RELEASE_INFO | python3 -c "import sys, json; data = json.load(sys.stdin); print(next(asset['url'] for asset in data['assets'] if asset['name'] == '$ASSET_NAME'))")
 
 # Check if the asset URL is found
@@ -29,9 +31,24 @@ if [ -z "$ASSET_URL" ]; then
   exit 1
 fi
 
-# Add accepts header to the request
-curl -H "Accept: application/octet-stream" -fsSL "${ASSET_URL}" -o "${SM_PATH}"
+TEMP_FILE=$(mktemp)
 
+# Add accepts header to the request
+curl -H "Accept: application/octet-stream" -fsSL "${ASSET_URL}" -o "${TEMP_FILE}"
+
+# Check if the file was downloaded successfully
+if [ ! -s "${TEMP_FILE}" ]; then
+  echo "Failed to download the asset: ${ASSET_URL}"
+  exit 1
+fi
+
+# Create the directory if it doesn't exist
+mkdir -p "${SM_DIR}"
+
+# Move the file to the correct location
+mv "${TEMP_FILE}" "${SM_PATH}"
+
+# Make the file executable
 chmod +x "${SM_PATH}"
 
 mkdir -p "$HOME/.config" "$HOME/.config/secrets-machine"
